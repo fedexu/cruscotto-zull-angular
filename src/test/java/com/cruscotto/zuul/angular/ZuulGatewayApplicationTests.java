3c9919b023733d92943463e61c9169670ee60a90
Federico Peruzzi <federico.xu125@gmail.com>
2021-03-01 15:09:53 +0100

    first commit

---
+++ src/test/java/com/cruscotto/zuul/angular/ZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cruscotto.zuul.angular;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
